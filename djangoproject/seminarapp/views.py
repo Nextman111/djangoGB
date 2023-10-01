eea8593fed98904ada062cf0f019fc6b068b82e8
VladislavRazvodov <g-man-111@ya.ru>
2023-10-01 18:49:42 +0300

    add and config logger

--- djangoproject/seminarapp/views.py
+++ djangoproject/seminarapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
+import logging
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     return HttpResponse("Hello, World!")
 
 
 def orelreshka(request):
-    return HttpResponse(f"{'орел' if randint(0,1) else 'решка'}")
+    res = f"{'орел' if randint(0,1) else 'решка'}"
+    logger.info(res)
+    return HttpResponse(res)
 
 
 def kosti(request):
