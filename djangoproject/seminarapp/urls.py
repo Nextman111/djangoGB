a0d76be40245ca71fa84ff1f48a8e261779a3c5a
VladislavRazvodov <g-man-111@ya.ru>
2023-10-01 18:13:41 +0300

    add pages orelreshka kosti and random_number

--- djangoproject/seminarapp/urls.py
+++ djangoproject/seminarapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, orelreshka, kosti, random_number
 
 
 urlpatterns = [
     path('', index, name="index"),
+    path('orelreshka/', orelreshka, name="orel ili reshka"),
+    path('kosti/', kosti, name="kosti"),
+    path('random_number', random_number, name="random number"),
 ]
--- djangoproject/seminarapp/views.py
+++ djangoproject/seminarapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from random import randint
 
 def index(request):
     return HttpResponse("Hello, World!")
+
+
+def orelreshka(request):
+    return HttpResponse(f"{'орел' if randint(0,1) else 'решка'}")
+
+
+def kosti(request):
+    return HttpResponse(f"{randint(1,6)}")
+
+
+def random_number(request):
+    return HttpResponse(f"{randint(0,100)}")
